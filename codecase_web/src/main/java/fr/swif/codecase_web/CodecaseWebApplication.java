b79b7ac26d083342ea34906781a9a3128539363c
Alexandre Calderoli <alexandrecalderoli96@gmail.com>
2026-06-07 21:52:48 +0200

    initial commit

---
+++ codecase_web/src/main/java/fr/swif/codecase_web/CodecaseWebApplication.java
@@ -0,0 +1,13 @@
+package fr.swif.codecase_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecaseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecaseWebApplication.class, args);
+	}
+
+}
---
+++ codecase_web/src/test/java/fr/swif/codecase_web/CodecaseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.swif.codecase_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecaseWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
