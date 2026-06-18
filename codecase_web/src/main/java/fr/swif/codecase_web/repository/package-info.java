b716ec33c3863d6a08164918abb469129e07045c
Alexandre Calderoli <alexandrecalderoli96@gmail.com>
2026-06-18 16:57:09 +0200

    <DOC> Création des package-info dans tous les packages

---
+++ codecase_web/src/main/java/fr/swif/codecase_web/repository/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * fr.swif.codecase_web.repository
+ * <hr>
+ * <p>Package-info de repository</p>
+ *
+ * @author Calderoli Alexandre
+ * @version 0.0.1
+ * @since 18/06/2026
+ */
+package fr.swif.codecase_web.repository;
