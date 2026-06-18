bed26920450c264098ff6545a716f24d9fd6e167
Alexandre Calderoli <alexandrecalderoli96@gmail.com>
2026-06-18 16:34:12 +0200

    <FEAT> Création et complétion de CustomProperties dans le package config + changement du nom de la clé de configuration dans application.properties de codecase_web à codecase-web

---
+++ codecase_web/src/main/java/fr/swif/codecase_web/config/CustomProperties.java
@@ -0,0 +1,27 @@
+package fr.swif.codecase_web.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * CustomProperties
+ * <i>de fr.swif.codecase_web.config</i>
+ * <hr>
+ * <p></p>
+ *
+ * @author Calderoli Alexandre
+ * @version 0.0.1
+ * @since 18/06/2026
+ */
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "fr.swif.codecase-web.appweb")
+public class CustomProperties {
+
+  /**
+   * L'URL vers l'API
+   */
+  private String apiUrl;
+}
